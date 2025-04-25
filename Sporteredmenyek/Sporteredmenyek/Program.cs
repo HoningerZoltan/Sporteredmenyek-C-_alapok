9a98166d68deb9c26b95e4e42504d75f7431918c
HoningerZ <honinger.zoltan2003@gmail.com>
2025-04-25 20:11:19 +0200

    projekt létrehozva

---
+++ Sporteredmenyek/Sporteredmenyek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sporteredmenyek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
